f6803bf0da7dbe319aacd3b60635586338c1a5a5
bjarkeck <bjarkeelias@gmail.com>
2013-12-03 16:51:36 +0100

    Bullet stuff

--- GiveUp/GiveUp/Classes/GameObjects/Obstacles/Cannon/AttachedCannon.cs
+++ GiveUp/GiveUp/Classes/GameObjects/Obstacles/Cannon/AttachedCannon.cs
@@ -21,19 +21,17 @@ namespace GiveUp.Classes.GameObjects.Obstacles
         Rectangle rectangle { get; set; }
 
         public Vector2 cannonPosition;
-        float minRotation = 1.3033f;
-        float maxRotation = 4.96f;
         float cannonRotation = 10;
         TimeSpan fireRate = TimeSpan.FromMilliseconds(1000);
         TimeSpan timeElapsed;
-
+        List<Rectangle> boxTiles;
         List<CannonBullet> cannonBullets = new List<CannonBullet>();
-        
+
         public override void Initialize(ContentManager content, Vector2 position)
         {
             bulletTexture = content.Load<Texture2D>("Images/Obstacles/AttachedCannon/bullet1");
 
-            List<Rectangle> boxTilesboxTiles = LevelManager.GameObjects.Where(x => x.GetType().Name == "BoxTile").Select(x => ((BoxTile)x).Rectangle).ToList();
+            boxTiles = LevelManager.GameObjects.Where(x => x.GetType().Name == "BoxTile").Select(x => ((BoxTile)x).Rectangle).ToList();
             var boxTile = LevelManager.GameObjects.Where(x => x.GetType().Name == "BoxTile").Select(x => ((BoxTile)x).Position);
             cannonTexture = content.Load<Texture2D>("Images/Obstacles/AttachedCannon/AttatchedCannonCannon");
 
@@ -43,15 +41,11 @@ namespace GiveUp.Classes.GameObjects.Obstacles
                 position.Y += 32 - texture.Height;
 
                 cannonPosition = new Vector2(position.X + 16, position.Y + 7);
-                maxRotation -= 1.5707f;
-                minRotation -= 1.5707f;
             }
             else if (boxTile.Any(x => x.X == position.X && x.Y == position.Y - 32))
             {
                 texture = content.Load<Texture2D>("Images/Obstacles/AttachedCannon/AttatchedCannonBodyB");
                 cannonPosition = new Vector2(position.X + 16, position.Y);
-                maxRotation += 1.5707f;
-                minRotation += 1.5707f;
             }
             else if (boxTile.Any(x => x.X == position.X - 32 && x.Y == position.Y))
             {
@@ -65,16 +59,12 @@ namespace GiveUp.Classes.GameObjects.Obstacles
                 position.X += 32 - texture.Width;
 
                 cannonPosition = new Vector2(position.X + 10, position.Y + 16);
-                maxRotation += 1.5707f * 2;
-                minRotation += 1.5707f * 2;
             }
             else
             {
                 //Hvis den flyver i luften
                 texture = content.Load<Texture2D>("Images/Obstacles/AttachedCannon/AttatchedCannonBodyL");
             }
-
-            this.cannonRotation = minRotation;
 
             this.Position = position;
             this.rectangle = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
@@ -87,12 +77,10 @@ namespace GiveUp.Classes.GameObjects.Obstacles
                 ,
                 Convert.ToDouble(cannonPosition.X - (Player.Rectangle.Origin().X))
                 ) + 3.1416f;
-            
+
             if (GameLogic.IsLineOfSight(10200, cannonPosition, Player.Rectangle))
             {
-                
-                if (rotation < minRotation || rotation > maxRotation)
-                    cannonRotation = rotation;
+                cannonRotation = rotation;
 
                 if ((timeElapsed += gameTime.ElapsedGameTime) > fireRate)
                 {
@@ -100,10 +88,13 @@ namespace GiveUp.Classes.GameObjects.Obstacles
                     cannonBullets.Add(new CannonBullet(bulletTexture, cannonPosition, rotation, 10));
                 }
             }
-
-            foreach (var bullet in cannonBullets)
+                                                //.ToList() så vi kan fjerne dem mens vi går igennemListen
+            foreach (CannonBullet bullet in cannonBullets)
             {
                 bullet.Update(gameTime, Player, LevelManager);
+                
+                //Lav collision her...foreach boxTile... dvs at bullets skal have en public Rectangle.
+
             }
 
             if (HandleCollision.PerPixesCollision(ref Player.Rectangle, rectangle, texture, ref Player.Velocity, ref Player.Position))
