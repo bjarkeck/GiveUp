d577b99e48c96f4da10b73537029d4fabe5f4276
Purup <jpurup@hotmail.com>
2013-12-07 18:31:08 +0100

    Menu pooop

--- GiveUp/GiveUp/Classes/Screens/MenuScreen.cs
+++ GiveUp/GiveUp/Classes/Screens/MenuScreen.cs
@@ -10,51 +10,71 @@ using System.Text;
 
 namespace GiveUp.Classes.Screens
 {
-    public class MenuScreen : BaseScreen
+
+
+    public abstract class MenuScreen : BaseScreen
     {
-        private static List<Button> buttons = new List<Button>();
-
-        public Texture2D Texture;
-        public Rectangle menuRectangle;
-        public Texture2D testTexture;
-        public Rectangle testRectangle;
-
+        private List<Button> buttons = new List<Button>();
+        private Texture2D bg;
+        private Texture2D topBarTexture;
+        private int buttonsWidth;
+        private int startPosition;
+        float buttonSizeScaleFactor;
+        public Type ActiveScreenType;
 
         public override void LoadContent()
         {
-            Texture = Content.Load<Texture2D>("Images/Menu/BackGround/MenuScreen");
-            testTexture = Content.Load<Texture2D>("Images/Tiles/IceGround");
-            testRectangle = new Rectangle(0, 0, testTexture.Width, testTexture.Height);
-            menuRectangle = new Rectangle(0, 0, 1600, 900);
-            buttons.Add(new Button(Content.Load<Texture2D>("Images/Tiles/ground"), new Vector2(0, 0), StartGame));
+            bg = Content.Load<Texture2D>("Images/Menu/BackGround/menuBg");
+
+            topBarTexture = Content.Load<Texture2D>("Images/Menu/BackGround/btnBarBg");
+
+            //Udregn scaleringsfakotr
+            buttonSizeScaleFactor = 74f / topBarTexture.Height;
+
+            //Tilføj knapperC:\Users\Purup\Documents\GitHub\GiveUp\GiveUp\GiveUp\Content\Images\Menu\Buttons\btnExitStatic.png
+            buttons.Add(new Button(Content, "Images/Menu/Buttons/btnPlay", () => { ScreenManager.Current.LoadScreen(new GameScreen()); }, buttonSizeScaleFactor, ActiveScreenType == typeof(MenuPlayScreen)));
+            buttons.Add(new Button(Content, "Images/Menu/Buttons/btnOnline", () => { ScreenManager.Current.LoadScreen(new MenuOnlineScreen()); }, buttonSizeScaleFactor, ActiveScreenType == typeof(MenuOnlineScreen)));
+            buttons.Add(new Button(Content, "Images/Menu/Buttons/btnSettings", () => { ScreenManager.Current.LoadScreen(new MenuSettingsScreen()); }, buttonSizeScaleFactor, ActiveScreenType == typeof(MenuSettingsScreen)));
+            buttons.Add(new Button(Content, "Images/Menu/Buttons/btnExit", () => { ScreenManager.Current.LoadScreen(new MenuExitScreen()); }, buttonSizeScaleFactor, ActiveScreenType == typeof(MenuExitScreen)));
+
+            //Udregn alle knappers bredde:
+            buttonsWidth = buttons.Sum(x => x.ButtonRectangle.Width);
+
+            //Udregn start position:
+            startPosition = (int)((1600 - buttonsWidth) / 2f);
+
+            int currentXOffset = 0;
+            foreach (var item in buttons)
+            {
+                item.ButtonRectangle.X = startPosition + currentXOffset;
+                currentXOffset += item.ButtonRectangle.Width;
+            }
+
             base.LoadContent();
         }
 
-        public static void StartGame()
-        {
-            ScreenManager.Current.LoadScreen(new GameScreen());
-        }
-        
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
             foreach (Button b in buttons)
             {
                 b.Update(gameTime);
             }
-            
+
             //Loads gameScreen
             //ScreenManager.Current.LoadScreen(new GameScreen());
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(Texture, menuRectangle, Color.White);
+            spriteBatch.Draw(bg, new Rectangle(0, 0, 1600, 900), Color.White);
+            spriteBatch.Draw(topBarTexture, new Rectangle(0, 0, startPosition, (int)(topBarTexture.Height * buttonSizeScaleFactor)), Color.White);
+            spriteBatch.Draw(topBarTexture, new Rectangle(startPosition + buttonsWidth, 0, startPosition, (int)(topBarTexture.Height * buttonSizeScaleFactor)), Color.White);
 
             foreach (Button b in buttons)
             {
                 b.Draw(spriteBatch);
             }
-            
+
             base.Draw(spriteBatch);
         }
     }
