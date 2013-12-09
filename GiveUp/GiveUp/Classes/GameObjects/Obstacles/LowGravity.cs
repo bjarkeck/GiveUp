f42ca8b75931c528ddc6e50bc6f6ac989d026a44
Purup <jpurup@hotmail.com>
2013-12-09 22:05:08 +0100

    dfdsfdsf

---
+++ GiveUp/GiveUp/Classes/GameObjects/Obstacles/LowGravity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GiveUp.Classes.GameObjects.Obstacles
+{
+    public class LowGravity : GameObject, IGameObject
+    {
+        public const char TileChar = 'o';
+
+
+        public override void Initialize(Microsoft.Xna.Framework.Content.ContentManager content, Microsoft.Xna.Framework.Vector2 position)
+        {
+            Player.Gravity = new Microsoft.Xna.Framework.Vector2(0, 0.2f);
+        }
+
+    }
+}
