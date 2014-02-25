34309f28732ed80dc581cd44aaa2deb4f2c64167
Purup <jpurup@hotmail.com>
2014-02-25 09:56:13 +0100

    no message

--- GiveUp/GiveUp/Classes/Core/Editor.cs
+++ GiveUp/GiveUp/Classes/Core/Editor.cs
@@ -14,7 +14,7 @@ namespace GiveUp.Classes.Core
 {
     public class Editor
     {
-        public static bool IsEnable = true;
+        public static bool IsEnable = false;
 
         private int selectedTile = -1;
         public LevelManagerr LevelManager
