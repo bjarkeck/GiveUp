fb36d91d8df1185047375a0436a11f50c5921e32
bjarkeck <bjarkeelias@gmail.com>
2014-02-28 20:25:38 +0100

    no message

--- GiveUp/GiveUp/Classes/LevelManager/LevelManager.cs
+++ GiveUp/GiveUp/Classes/LevelManager/LevelManager.cs
@@ -238,6 +238,8 @@ namespace GiveUp.Classes.LevelManager
 
             if (PracticeRun == false && runCompleted)
             {
+                Player.ParticleManager.ParticleEmitters["Blood"].Particles.Clear();
+                Player.ParticleManager.ParticleEmitters["Blood"].MaxNumberOfParitcles = 0;
                 CurrentSubLevel += 1;
                 if (Levels.Count() < CurrentSubLevel)
                 {
@@ -264,8 +266,8 @@ namespace GiveUp.Classes.LevelManager
                 item.CollisionLogic();
             }
 
-            LevelTimer += (int)((float)gameTime.ElapsedGameTime.Milliseconds * Time.GameSpeed);
-            ChallengeTimer += (int)((float)gameTime.ElapsedGameTime.Milliseconds * Time.GameSpeed);
+            LevelTimer += (int)((float)gameTime.ElapsedGameTime.Milliseconds);
+            ChallengeTimer += (int)((float)gameTime.ElapsedGameTime.Milliseconds);
 
             if (changeLevel)
             {
